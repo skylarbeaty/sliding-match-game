ef118142cb07de0021f82534fe51a70b37e1e610
William Beaty <31560421+wbeaty@users.noreply.github.com>
2019-03-19 13:03:20 -0400

    wrapping in
    tiles now wrap around the outside  when sliding

--- SlidingMatchGame/Assets/Tile.cs
+++ SlidingMatchGame/Assets/Tile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour {
+	static bool draggingHappening = false;
 	public enum tyleType {red, purple};
 	public tyleType type = tyleType.red;
 	bool falling = true;
@@ -21,8 +22,8 @@ public class Tile : MonoBehaviour {
 	List<Tile> dragGroup;
 	
 	void Start(){
-		rightEdge = leftEdge + width * cellSize;
-		topEdge = bottomEdge + height * cellSize;
+		rightEdge = leftEdge + (width - 1) * cellSize;
+		topEdge = bottomEdge + (height - 1) * cellSize;
 		dragGroup = new List<Tile>();
 	}
 	void Update(){
@@ -43,6 +44,8 @@ public class Tile : MonoBehaviour {
 	}
 
 	void CheckBellow(){
+		if (draggingHappening)//dont do drops while dragging
+			return;
 		Vector2 point = (Vector2) transform.position + Vector2.down * cellSize;
 		Collider2D other = Physics2D.OverlapCircle(point, 0.2f, mask);
 		if (other == null)
@@ -62,6 +65,19 @@ public class Tile : MonoBehaviour {
 		xPos = Mathf.Round(transform.position.x * 2.0f) / 2.0f;
 		yPos = Mathf.Round(transform.position.y * 2.0f) / 2.0f;
 		transform.position = new Vector3(xPos,yPos,transform.position.z);
+		//check for outside grid
+		if (transform.position.x < leftEdge){
+			transform.position = transform.position + Vector3.right * width * cellSize;
+		}
+		if (transform.position.x > rightEdge){
+			transform.position = transform.position - Vector3.right * width * cellSize;
+		}
+		if (transform.position.y < bottomEdge){
+			transform.position = transform.position + Vector3.up * height * cellSize;
+		}
+		if (transform.position.y > topEdge){
+			transform.position = transform.position - Vector3.up * height * cellSize;
+		}
 	}
 
 	
@@ -77,6 +93,7 @@ public class Tile : MonoBehaviour {
 		print("MOUSE UP");
 		clicked = false;
 		dragging = false;
+		draggingHappening = false;
 		foreach(Tile tile in dragGroup){
 			tile.SnapToGrid();
 		}
@@ -88,6 +105,7 @@ public class Tile : MonoBehaviour {
 		Vector3 mouseCur = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		Vector3 mouseDelta = mouseCur - mouseStart;
 
+		//start drag
 		float dragThres = 0.3f;
 		if (!dragging && (Mathf.Abs(mouseDelta.x) > dragThres || Mathf.Abs(mouseDelta.y) > dragThres)){
 			print("DRAG TRIGGERED");
@@ -97,6 +115,7 @@ public class Tile : MonoBehaviour {
 			else
 				dragDir = Vector3.up;
 			dragging = true;
+			draggingHappening = true;
 
 			//get drag group
 			Collider2D[] colls;
@@ -104,24 +123,38 @@ public class Tile : MonoBehaviour {
 			size += (Vector2) dragDir * width * cellSize * 2.0f; //make wide on corect side
 			colls = Physics2D.OverlapBoxAll(transform.position, size, 0.0f, mask);
 			print("Colls length: " + colls.Length);
-			foreach (Collider2D coll in colls)
-			{
+			foreach (Collider2D coll in colls){ //add all in line to group
 				if (coll == null)
 					continue;
 				Tile tile = coll.gameObject.GetComponent<Tile>();
-				if (object.ReferenceEquals(tile,null)){
-					print("NULL");
-					continue;
-				}
 				tile.dragStart = tile.transform.position;
 				dragGroup.Add(tile);
 			}
 		}
-
+		//dragging step
 		if (dragging){
 			Vector3 dragDelta = new Vector3(mouseDelta.x * dragDir.x, mouseDelta.y * dragDir.y, 0.0f);
 			foreach(Tile tile in dragGroup){
+				//move the tile
 				tile.transform.position = tile.dragStart + dragDelta;
+				//warp around hor
+				if (tile.transform.position.x < leftEdge - 0.49){
+					tile.transform.position = tile.transform.position + Vector3.right * width * cellSize;
+					tile.dragStart = tile.transform.position - dragDelta;
+				}
+				if (tile.transform.position.x > rightEdge + 0.49){
+					tile.transform.position = tile.transform.position - Vector3.right * width * cellSize;
+					tile.dragStart = tile.transform.position - dragDelta;
+				}
+				//warp around vert
+				if (tile.transform.position.y < bottomEdge - 0.49){
+					tile.transform.position = tile.transform.position + Vector3.up * height * cellSize;
+					tile.dragStart = tile.transform.position - dragDelta;
+				}
+				if (tile.transform.position.y > topEdge + 0.49){
+					tile.transform.position = tile.transform.position - Vector3.up * height * cellSize;
+					tile.dragStart = tile.transform.position - dragDelta;
+				}
 			}
 		}
 	}
