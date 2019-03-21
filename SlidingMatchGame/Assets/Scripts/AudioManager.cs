c75d0ac8942f5374449fa54a9530ffef9d0d908e
William Beaty <31560421+wbeaty@users.noreply.github.com>
2019-03-21 19:54:16 -0400

    added audio toggle

--- SlidingMatchGame/Assets/Scripts/AudioManager.cs
+++ SlidingMatchGame/Assets/Scripts/AudioManager.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour {
 	public Sound[] fall;
 	public Sound[] match;
 	public Sound[] land;
 	public Sound timerEnd;
-
+	static bool soundOn = true;
+	public Image soundIcon;
+	public Sprite soundSpriteOn, soundSpriteOff;
 	void Start(){
 		foreach(Sound sound in fall){SetupSound(sound);}
 		foreach(Sound sound in match){SetupSound(sound);}
@@ -27,14 +30,27 @@ public class AudioManager : MonoBehaviour {
 		// return;
 	}
 	public void PlayMatch(){
+		if (!soundOn)
+			return;
 		int index = Mathf.FloorToInt(Random.value * (match.Length - 1));
 		match[index].source.Play();
 	}
 	public void PlayLand(){
+		if (!soundOn)
+			return;
 		int index = Mathf.FloorToInt(Random.value * (land.Length - 1));
 		land[index].source.Play();
 	}
-	public void PlayTimer(){
+	public void PlayTimer(){if (!soundOn)
+			return;
 		timerEnd.source.Play();
+	}
+
+	public void SoundFlip(){
+		soundOn = !soundOn;
+		if (soundOn)
+			soundIcon.sprite = soundSpriteOn;
+		else 
+			soundIcon.sprite = soundSpriteOff;
 	}
 }
